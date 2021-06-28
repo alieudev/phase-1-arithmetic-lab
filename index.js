7529d38415d903ce1a9c90cc3643954fa6963006
Alieu Baldeh <alieu.dev@gmail.com>
2021-06-28 19:30:37 -0400

    commit tree 6f897632d27efb55aff825660eab130ad027b1b0

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 10);
 
-// Write your code here
+const num3 = 10;
+const num4 = 6;
+const mod = num3 % num4;
+const max = Math.max(1, 4, 10, 20);
+
+// const num1 = 2;
+// const num2 = 31;
+// const multiply = num1 * num2;
+// const random = Math.floor( Math.random() * 1000 ) + 1;
+// const num3 = 16;
+// const num4 = 6;
+// const mod = num3 % num4;
+// const max = Math.max( 5, 10, 15, 20 );
